7005c5b6527c61993c4c266e2e3c073f9797a609
Your Name <you@example.com>
2022-04-03 18:01:51 +0300

    Trivia_Game: With 6 Questions:

--- Questions & Answers/File1.py
+++ Questions & Answers/File1.py
@@ -1,12 +1,3 @@
-def start_game():
-    print('-----Welcome To Trivia Game ☻-----\n-----There is 9 Questions and One is Bonus!-----')
-    start = int(input('To Start Press 1, Press 2 To Exit: '))
-    while start != 1:
-        start = int(input('Wrong Number, Press 1 Or 2:'))
-        if start == 2:
-            print('GoodBye !')
-            break
-
 
 def questions_on_sport():
     counter = 0
