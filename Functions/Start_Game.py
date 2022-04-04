a1edecb21014d75c02f3b62e81864a5dead2e611
Your Name <you@example.com>
2022-04-04 15:40:52 +0300

    Trivia_Game: With 10 Questions: Functions: Start_Game Finish ! # New Branch

--- Functions/Start_Game.py
+++ Functions/Start_Game.py
@@ -16,7 +16,8 @@ def start_the_game():
         print('------------------------')
         st = bonus_question()
         if st >= 100:
-            print(user, 'You The Best ')
+            print(user, 'You The Best ! ')
         else:
             print(user, 'You can do better than that')
         return st
+
--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from Functions.Start_Game import start_the_game
 def main_trivia_game():
-    w = start_the_game()
-    return w
+    begin = start_the_game()
+    return begin
+
 
 main_trivia_game()
